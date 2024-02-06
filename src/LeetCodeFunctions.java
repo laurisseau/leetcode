f6e721cc9e3adf56e6691f49ae0d231b3ab5a548
laurisseau Joseph <reso0208@gmail.com>
2024-02-05 20:36:14 -0500

    commit

--- src/LeetCodeFunctions.java
+++ src/LeetCodeFunctions.java
@@ -1,6 +1,47 @@
 public class LeetCodeFunctions {
-    public int addFunc(int a, int b){
-        return a + b;
+    public int trap(int[] height) {
+        /*
+            Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+            Output: 6
+         */
+        int n = height.length;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        int water = 0;
+
+        for(int i = 0; i < n; i++){
+            if(i == 0){
+                leftMax[0] = height[0];
+                rightMax[n - 1] = height[n - 1];
+            }else{
+                leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+                rightMax[n - i - 1] = Math.max(height[n - i - 1], rightMax[n - i]);
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            water += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+
+        return water;
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while(j >= 0){
+            if(i >= 0 && nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                k--;
+                i--;
+            }else{
+                nums1[k] = nums2[j];
+                k--;
+                j--;
+            }
+        }
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     static LeetCodeFunctions leetCodeFunctions = new LeetCodeFunctions();
     public static void main(String[] args) {
-        System.out.println(leetCodeFunctions.addFunc(1, 3));
+        System.out.println(leetCodeFunctions.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
     }
 }
 
