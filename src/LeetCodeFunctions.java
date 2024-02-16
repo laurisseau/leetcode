221492eb5952da2b3bc345cd68d8d7c174f096b3
laurisseau Joseph <reso0208@gmail.com>
2024-02-15 21:37:46 -0500

    commit

--- src/LeetCodeFunctions.java
+++ src/LeetCodeFunctions.java
@@ -87,5 +87,19 @@ public class LeetCodeFunctions {
         return candies;
     }
 
+    public int removeDuplicates(int[] nums) {
+        if (nums.length <= 2) {
+            return nums.length;
+        }
+
+        int index = 2;
+        for(int i = 2; i < nums.length; i++){
+            if(nums[i] != nums[index - 2]){
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+
 
 }
